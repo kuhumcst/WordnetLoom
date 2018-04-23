9523a7d980357bb04c429106e85205dfe7613118
Mitchell John Seaton <seaton@hum.ku.dk>
2018-04-23 14:35:36 +0200

    flyway migration

--- wordnetloom-service/src/main/resources/db/migration/V1_7__User_Settings.sql
+++ wordnetloom-service/src/main/resources/db/migration/V1_7__User_Settings.sql
@@ -14,7 +14,7 @@ REFERENCES users (id);
 INSERT INTO users_settings (user_id, lexicon_marker, chosen_lexicons, show_tool_tips)
   SELECT
     u.id,
-    TRUE,
+    FALSE,
     '1;2;',
     TRUE
   FROM users u;
